497b2a38357cd83e296a0e74335a6f6ce0bc9f40
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-11 09:04:44 +0100

    Initial commit with basic dataset characteristics

---
+++ flowers.py
@@ -0,0 +1,30 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# shape of the dataset
+print(dataset.shape)
+
+# can peak at the data with head
+print(dataset.head(20))
+
+# we can even get a statistical summary
+print(dataset.describe())
